da07136231f966a39f1158bd0dcf1407ff74d3e8
vrMithun <mithuncsegvsraja@gmail.com>
2024-09-08 19:58:19 +0530

    sign

--- inference_classifier.py
+++ inference_classifier.py
@@ -7,7 +7,7 @@ import numpy as np
 model_dict = pickle.load(open('./model.p', 'rb'))
 model = model_dict['model']
 
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
